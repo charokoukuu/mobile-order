6988d46f0c82f76c7b6ae604e19697d67fc335a8
nikudango <satoru_rutosa@icloud.com>
2022-06-25 00:19:34 +0900

    認証mailが遅れない

--- src/Register.tsx
+++ src/Register.tsx
@@ -6,28 +6,66 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { ChangeEvent, useState } from "react";
-
+import { ChangeEvent, useEffect, useState } from "react";
+import {
+  getAuth,
+  fetchSignInMethodsForEmail,
+  EmailAuthProvider,
+  sendSignInLinkToEmail,
+} from "firebase/auth";
 const theme = createTheme();
 
 export const Register = () => {
+  const auth = getAuth();
   const [email, setEmail] = useState("");
   const [isAddress, setIsAddress] = useState(false);
+
+  // 認証セッティング
+  const actionCodeSettings = {
+    // リダイレクト先としたいURL。
+    // URL は、Firebase Console の authorized domains list に含まれている必要があります。
+    url: "http://mobile-order-4d383.web.app",
+    // This must be true.
+    handleCodeInApp: true,
+    iOS: {
+      bundleId: "com.example.ios",
+    },
+    android: {
+      packageName: "com.example.android",
+      installApp: true,
+      minimumVersion: "12",
+    },
+    dynamicLinkDomain: "example.page.link",
+  };
+
   // テキストフィールドに入力してる時の処理
   const handleChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
-    const regex = /[a-zA-Z0-9._-]@oit.ac.jp$/;
-    setIsAddress(regex.test(email));
   };
+
+  // 正規表現でemailをチェック
+  useEffect(() => {
+    const regex = /^e[a-zA-Z0-9._-]+@oit.ac.jp$/;
+    setIsAddress(regex.test(email));
+  }, [email]);
+
+  const sendAsycEmail = async () => {
+    try {
+      // await sendSignInLinkToEmail(auth, email, actionCodeSettings);
+      // window.localStorage.setItem("emailForSignIn", email);
+      await sendSignInLinkToEmail(auth, email, actionCodeSettings);
+      window.localStorage.setItem("emailForSignIn", email);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   // SIGIN IN押したときの処理
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log({
-      email: email,
-    });
     console.log(email);
     // 正規表現で大工大生かどうかチェック
-    isAddress ? console.log("ok") : console.log("no");
+    isAddress ? sendAsycEmail() : console.log("no");
   };
 
   return (
