6b2bc3a8c8fc0dc45ceb539f6f09fb6f8499e940
charokoukuu <charokoukuu@gmail.com>
2022-06-26 03:31:08 +0900

    DetailDialogのチェックボックス追加。値段を管理するuseContextの追加

--- src/component/DetailDialog.tsx
+++ src/component/DetailDialog.tsx
@@ -1,8 +1,10 @@
 import { Button, Dialog, Grid } from "@mui/material";
 import { MenuData } from "../Interface";
 import "../App.css"
-import { useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import React from "react";
+const menuData = createContext<any>(null);
+const totalPrice = createContext<number>(0);
 
 interface DetailDialogProps {
     open: boolean;
@@ -14,68 +16,81 @@ interface DetailDialogProps {
 
 
 export const DetailDialog = (props: DetailDialogProps) => {
-
     const [menu, setMenu] = useState<MenuData | undefined>(props.menu);
-    const menuData = React.createContext<any>(menu);
+    const value = { menu, setMenu }
+    useEffect(() => {
+        setMenu(props.menu);
+    }, [props.menu])
     return (
         <div >
-            <menuData.Provider value={{ menu, setMenu }}>
-                <Dialog
-                    open={props.open}
-                    onClose={props.onPrev}
-                    PaperProps={{
-                        style: {
-                            backgroundColor: '#EFEFEF',
-                            borderRadius: '13px',
-                        },
-                    }}
+            <menuData.Provider value={value}>
+                <totalPrice.Provider value={props.menu?.price || 0}>
+                    <Dialog
+                        open={props.open}
+                        onClose={props.onPrev}
+                        PaperProps={{
+                            style: {
+                                backgroundColor: '#EFEFEF',
+                                borderRadius: '13px',
+                            },
+                        }}
 
-                >
-                    <div style={{ width: "80vw" }}>
-                        <MaterialMenuCard menu={props.menu} />
-                        <MaterialSizeSelectCard menu={props.menu} />
-                        <div className="center themaFontColor" style={{ margin: "1vw 0", fontSize: "15vw" }}>{props.menu?.price}<span style={{ fontSize: "7vw" }}> 円</span></div>
-                        <div className="center">  <Button style={{ width: "70vw", backgroundColor: "#006C9B", height: "10vw", borderRadius: "11px" }} variant="contained" onClick={() => {
-                            props.onNext(props.menu)
-                        }} >
-                            カートに追加
-                        </Button></div>
-                        <div className="center" style={{ textDecoration: "underline #006C9B", margin: "2vw 0" }}><Button style={{ color: "#006C9B" }} onClick={props.onPrev}>閉じる</Button></div>
-                    </div>
-                </Dialog>
+                    >
+                        <div style={{ width: "80vw" }}>
+                            <MaterialMenuCard />
+                            <MaterialSizeSelectCard />
+                            <div className="center themaFontColor" style={{ margin: "1vw 0", fontSize: "15vw" }}>{value.menu?.price}<span style={{ fontSize: "7vw" }}> 円</span></div>
+                            <div className="center">  <Button style={{ width: "70vw", backgroundColor: "#006C9B", height: "10vw", borderRadius: "11px" }} variant="contained" onClick={() => {
+                                props.onNext(value.menu);
+                            }} >
+                                カートに追加
+                            </Button></div>
+                            <div className="center" style={{ textDecoration: "underline #006C9B", margin: "2vw 0" }}><Button style={{ color: "#006C9B" }} onClick={props.onPrev}>閉じる</Button></div>
+                        </div>
+                    </Dialog>
+                </totalPrice.Provider>
+
             </menuData.Provider>
+
         </div>
     );
 }
 
-interface MaterialCardProps {
-    menu: MenuData | undefined;
-}
-const MaterialMenuCard = (props: MaterialCardProps) => {
-    const [isSelected, setIsSelected] = useState(false);
+const MaterialMenuCard = () => {
+    const { menu } = useContext(menuData);
+
     return (
         <div style={{ backgroundColor: "#ffffff", margin: "3vw 3vw", boxShadow: "0px 3px 6px #00000029", borderRadius: "13px" }}>
             <div style={{ textAlign: "center", margin: "2vw 0", fontSize: "7vw" }}>
-                <div className="japanese_R" style={{ padding: "3vw 0" }}>{props.menu?.title}</div>
-                <div><img style={{ width: "50vw", borderRadius: "13px", margin: "1vw 0" }} src={props.menu?.image || ""} alt="menu" /></div>
-                <div className="japanese_L" style={{ textAlign: "center", fontSize: "3vw", width: "50vw", margin: "auto", paddingBottom: "7vw" }}>{props.menu?.description}</div>
+                <div className="japanese_R" style={{ padding: "3vw 0" }}>{menu?.title}</div>
+                <div><img style={{ width: "50vw", borderRadius: "13px", margin: "1vw 0" }} src={menu?.image || ""} alt="menu" /></div>
+                <div className="japanese_L" style={{ textAlign: "center", fontSize: "3vw", width: "50vw", margin: "auto", paddingBottom: "7vw" }}>{menu?.description}</div>
 
             </div>
         </div>
     )
 }
-const MaterialSizeSelectCard = (props: MaterialCardProps) => {
+const MaterialSizeSelectCard = () => {
+    const { menu } = useContext(menuData);
+
     return (
         <div style={{ backgroundColor: "#ffffff", margin: "0vw 3vw", boxShadow: "0px 3px 6px #00000029", borderRadius: "13px" }}>
             <div style={{ textAlign: "center", margin: "0vw 0", fontSize: "7vw" }}>
-                <div style={{ padding: "5vw 0" }}><SelectedCard menu={props.menu} /></div>
+                <div style={{ padding: "5vw 0" }}>
+                    {menu !== undefined && <SelectedCard price={menu.price} />}
+                </div>
             </div>
         </div>
     )
 }
 
-const SelectedCard = (props: MaterialCardProps) => {
+const SelectedCard = (props: { price: number }) => {
     const [isChecked, setIsChecked] = useState(false);
+    const { setMenu } = useContext(menuData);
+    const price = useContext(totalPrice);
+    useEffect(() => {
+        setMenu((prevState: any) => ({ ...prevState, price: isChecked ? price + 100 : price }));
+    }, [isChecked, price, setMenu])
     const defaultButton = {
         background: "#006C9B 0% 0% no-repeat padding-box",
         width: "31vw",
