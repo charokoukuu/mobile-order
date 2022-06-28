00214c959aa5d7e08d0a1a71af41cab6212ee052
charokoukuu <charokoukuu@gmail.com>
2022-06-29 03:54:20 +0900

    時間を比較しsortするロジックの作成

--- src/History.tsx
+++ src/History.tsx
@@ -7,36 +7,26 @@ import { UserInfo } from "./UserInfo";
 export const History = () => {
     const [oneOrderData, setOneOrderData] = useState<DocumentData[]>();
     const [isGetHistoryData, setIsGetHistoryData] = useState<boolean>(false);
-    const [sortOrderData, setSortOrderData] = useState<DocumentData[]>();
-
 
     useEffect(() => {
         (async () => {
-            setOneOrderData(await SearchCollectionDataGet("order", "user.uid", UserInfo.user.uid));
+            let order = await SearchCollectionDataGet("order", "user.uid", UserInfo.user.uid);
+            order?.sort((prev, curr) => {
+                if (prev.date.seconds > curr.date.seconds) return -1;
+                else if (prev.date.seconds < curr.date.seconds) return 1;
+                else return 0;
+            });
+            setOneOrderData(order);
             setIsGetHistoryData(true);
         })()
     }, [])
-    useEffect(() => {
-        let order = oneOrderData;
-        order?.sort((prev, curr) => {
-            if (prev.date.seconds > curr.date.seconds) {
-                return -1;
-            } else if (prev.date.seconds < curr.date.seconds) {
-                return 1;
-            } else {
-                return 0;
-            }
 
-        });
-        setSortOrderData(order);
-
-    }, [oneOrderData])
     return (
         <div >
             <h1>History</h1>
             {
                 isGetHistoryData ?
-                    sortOrderData?.map((e, i) => {
+                    oneOrderData?.map((e, i) => {
                         return (
                             <div style={{ margin: "20vw 0" }} key={i}>
                                 <div>{e.id}</div>
