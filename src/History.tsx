f344adf0873d45f25711def91a243b86ee1735a6
charokoukuu <charokoukuu@gmail.com>
2022-06-28 16:07:30 +0900

    履歴表示ロジック

--- src/History.tsx
+++ src/History.tsx
@@ -1,19 +1,47 @@
 import { User } from "@firebase/auth";
+import { DocumentData } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { GetUserInfo } from "./SubmitGet";
+import { MenuData } from "./Interface";
+import { GetUserInfo, SearchCollectionDataGet } from "./SubmitGet";
 
 export const History = () => {
     const [user, setUser] = useState<User>();
+    const [oneOrderData, setOneOrderData] = useState<DocumentData[]>();
 
     useEffect(() => {
         GetUserInfo((e) => {
             setUser(e);
-        })
+        });
+
     }, [])
+    useEffect(() => {
+        (async () => {
+            user && setOneOrderData(await SearchCollectionDataGet("order", "user.uid", user.uid));
+        })()
+    }, [user])
+    useEffect(() => {
+        let test: any = oneOrderData?.map(e => e.menu).flatMap(e => e).map(e => e.title);
+        console.log(oneOrderData);
+    }, [oneOrderData])
     return (
-        <div>
+        <div >
             <h1>History</h1>
-            <h1>{user?.uid}</h1>
+            {
+                oneOrderData?.map((e, i) => {
+                    return (
+                        <div style={{ margin: "20vw 0" }} key={i}>
+                            <p>{e.id}</p>
+                            <p>{e.user.uid}</p>
+                            <div>{e.menu.map((e: any, i: number) => {
+                                return <p key={i}> {e.title}</p>
+                            })}</div>
+                            <p>{e.date.toString()}</p>
+                        </div>
+                    )
+                }
+                )
+            }
+
         </div>
     )
 }
