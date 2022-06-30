8887cac64add17bfc9ca6f07322a41621a97f05f
skazu37 <e1920037@oit.ac.jp>
2022-06-30 11:24:42 +0900

    カードで履歴を表示

--- src/History.tsx
+++ src/History.tsx
@@ -3,6 +3,12 @@ import { useEffect, useState } from "react";
 import { LoadingAnimation } from "./component/LoadingAnimation";
 import { SearchCollectionDataGet } from "./SubmitGet";
 import { UserInfo } from "./UserInfo";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { OrderCompleted } from "./OrderCompleted";
+// import dayjs from "dayjs";
+// import 'dayjs/locale/ja';
 
 export const History = () => {
     const [oneOrderData, setOneOrderData] = useState<DocumentData[]>();
@@ -20,6 +26,7 @@ export const History = () => {
             setIsGetHistoryData(true);
         })()
     }, [])
+    // console.log(oneOrderData);
 
     return (
         <div >
@@ -28,18 +35,32 @@ export const History = () => {
                 isGetHistoryData ?
                     oneOrderData?.map((e, i) => {
                         return (
-                            <div style={{ margin: "20vw 0" }} key={i}>
-                                <div>{e.id}</div>
-                                <div>{e.menu.map((e: any, i: number) => {
-                                    return <div key={i}> {e.title}</div>
-                                })}</div>
-                                <div>{e.date.toDate().toString()}</div>
+                            <div style={{ margin: "5vw 0" }} key={i}>
+                                <Card sx={{ minWidth: 275 }} onClick={()=>{
+                                   console.log(e.id);
+                                //    <OrderCompleted />
+                                }}>
+                                    <CardContent>
+                                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                        <div style={{textAlign:"right"}}>{e.date.toDate().toDateString()}</div>
+                                        </Typography>
+                                        <Typography variant="h5" component="div">
+                                        <div>{e.menu.map((e: any, i: number) => {
+                                            return <div key={i}> {e.title}</div>
+                                        })}</div>
+                                        <div style={{textAlign:"right"}}>￥{e.totalPrice}</div>
+                                        </Typography>
+                                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                        <div style={{textAlign:"right"}}>ID:{e.id}</div>
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
                             </div>
                         )
                     }) :
                     <LoadingAnimation />
             }
-
         </div>
     )
 }
+
