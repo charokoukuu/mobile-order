cedfff51560cf52633419d3e32cdd0102d567c02
nikudango <satoru_rutosa@icloud.com>
2022-07-05 02:48:03 +0900

    ログイン失敗画面修正

--- src/component/IllegalEmailAddress.tsx
+++ src/component/IllegalEmailAddress.tsx
@@ -3,34 +3,34 @@ import { deleteUser } from "firebase/auth";
 import { useEffect } from "react";
 import { auth } from "../Firebase";
 
-
 interface IllegalEmailAddressProps {
-    email: string;
-    onClick: () => void;
+  email: string;
+  onClick: () => void;
 }
 
 export const IllegalEmailAddress = (props: IllegalEmailAddressProps) => {
-
-    useEffect(() => {
-        const DeleteUserRedirect = () => {
-            if (auth.currentUser) {
-                deleteUser(auth.currentUser)
-            }
-        };
-        DeleteUserRedirect();
-        console.log(auth.currentUser);
-    }, [])
-    return (
-        <div>
-            <div style={{ textAlign: "center", margin: "5vw 0", fontSize: "7vw" }}>
-                <h1>大学のアカウントじゃないと無理だよ</h1>
-            </div>
-            <div style={{ textAlign: "center", margin: "5vw 0" }}>
-                <Button onClick={props.onClick}>
-                    再ログイン
-                </Button>
-
-            </div>
-        </div>
-    );
-}
+  useEffect(() => {
+    const DeleteUserRedirect = () => {
+      if (auth.currentUser) {
+        deleteUser(auth.currentUser);
+      }
+    };
+    DeleteUserRedirect();
+    console.log(auth.currentUser);
+  }, []);
+  return (
+    <div style={{ textAlign: "center", margin: "5vw 0" }}>
+      <h2 style={{ fontSize: "5vw" }}>
+        大学アカウントで
+        <br />
+        ログインしてください。
+      </h2>
+      <Button onClick={props.onClick} variant="outlined" color="error">
+        再ログイン
+      </Button>
+      <p>
+        何度もこの画面が表示される場合はブラウザのアカウントをご確認ください。
+      </p>
+    </div>
+  );
+};
