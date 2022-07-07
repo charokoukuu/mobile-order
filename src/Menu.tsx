f2b35eb1060b24f1a72bc9fe34a9df2c7da434e7
charokoukuu <charokoukuu@gmail.com>
2022-07-08 01:34:11 +0900

    FilterMenuData

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -11,13 +11,65 @@ import { Cart } from "./component/Cart";
 import { LoadingAnimation } from "./component/LoadingAnimation";
 import axios from "axios";
 import { auth } from "./Firebase";
+import SwipeableViews from 'react-swipeable-views';
+import AppBar from '@mui/material/AppBar';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
 const apiUrl = "https://pocketmansion.tk/"
 // const apiUrl = "http://localhost:3001/"
 const hostUrl = "http://localhost:3000";
 // const hostUrl = "https://mobile-order-4d383.web.app";
 export type CategoryProp = "メイン" | "ドリンク" | "トッピング";
+const menuCategoryArray: CategoryProp[] = ["メイン", "ドリンク", "トッピング"];
+interface TabPanelProps {
+    children?: React.ReactNode;
+    dir?: string;
+    index: number;
+    value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`full-width-tabpanel-${index}`}
+            aria-labelledby={`full-width-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+function a11yProps(index: number) {
+    return {
+        id: `full-width-tab-${index}`,
+        'aria-controls': `full-width-tabpanel-${index}`,
+    };
+}
+
 // type Mode = "menu" | "complete";
 export const Menu = () => {
+
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+
+    const handleChangeIndex = (index: number) => {
+        setValue(index);
+    };
     const [categoryMode, setCategoryMode] = useState<CategoryProp>("メイン");
     // const [mode, setMode] = useState<Mode>("menu");
     const [detailDialogOpen, setDetailDialogOpen] = useState(false);
@@ -43,40 +95,53 @@ export const Menu = () => {
     return (
         <div style={{ position: "relative" }}>
             {isGetMenu ? <div>
-                <Alert severity="error" onClick={() => {
+                {/* <Alert severity="error" onClick={() => {
                     window.location.href = "/history";
-                }}><div className="japanese_R">未受け取りの注文があります (詳細)</div></Alert>
-                <CategoryBar category={["メイン", "ドリンク", "トッピング"]} onClick={function (category: CategoryProp): void {
+                }}><div className="japanese_R">未受け取りの注文があります (詳細)</div></Alert> */}
+                {/* <CategoryBar category={["メイン", "ドリンク", "トッピング"]} onClick={function (category: CategoryProp): void {
                     setCategoryMode(category)
-                }} />
-                <Grid container >
-                    {menu.filter((item: any) => item.category === categoryMode && item.isStatus).map((menu: any, index: number) => {
-                        return (
-                            <Grid item key={index} style={{
-                                margin: "3vw auto"
-                            }}>
-                                <FoodCard menu={menu} onClick={function (): void {
-                                    menu.isBigSize === true && setChosenMenu({
-                                        title: menu.title,
-                                        description: menu.description,
-                                        price: menu.price,
-                                        id: menu.id,
-                                        image: menu.image,
-                                        category: menu.category,
-                                        isBigSize: menu.isBigSize,
-                                        bigSizeDiffPrice: menu.bigSizeDiffPrice,
-                                        isStatus: menu.isStatus,
-                                        isSale: menu.isSale,
-                                    });
-                                    menu.isBigSize === false && setChosenMenu(menu);
-                                    setDetailDialogOpen(true);
-                                }} />
+                }} /> */}
 
-                            </Grid>
-                        )
+
+                <AppBar position="static">
+                    <Tabs
+                        style={{
+                            backgroundColor: "#fff",
+                            color: "#000",
+                            fontSize: "1.5rem",
+                        }}
+                        value={value}
+                        onChange={handleChange}
+                        indicatorColor="primary"
+                        textColor="inherit"
+                        variant="fullWidth"
+                        aria-label="full width tabs example"
+                    >
+                        {
+                            menuCategoryArray.map((category, index) => {
+                                return (
+                                    <Tab key={category} label={category} {...a11yProps(index)} />
+                                )
+                            })
+                        }
+
+                    </Tabs>
+                </AppBar>
+                <SwipeableViews
+                    index={value}
+                    onChangeIndex={handleChangeIndex}
+                >
+                    {
+                        menuCategoryArray.map((category, index) => {
+                            return (
+                                <TabPanel key={category} value={value} index={index}>
+                                    <FilterMenuData menu={menu} categoryMode={category} setChosenMenu={setChosenMenu} setDetailDialogOpen={setDetailDialogOpen} />
+                                </TabPanel>
+                            )
+                        })
                     }
-                    )}
-                </Grid>
+                </SwipeableViews>
+
                 <div style={{ marginBottom: "13vw" }}>
                     <Order open={orderDialog} onDelete={(e, i) => {
                         setOrderData(orderData.filter((_, index) => index !== i))
@@ -130,3 +195,42 @@ export const Menu = () => {
         </div>
     );
 };
+
+interface FilterMenuDataProps {
+    menu: DocumentData[];
+    categoryMode: string;
+    setChosenMenu: (e: MenuData) => void;
+    setDetailDialogOpen: (e: boolean) => void;
+}
+const FilterMenuData = (props: FilterMenuDataProps) => {
+    return (
+        <Grid container >
+            {props.menu.filter((item: any) => item.category === props.categoryMode && item.isStatus).map((menu: any, index: number) => {
+                return (
+                    <Grid item key={index} style={{
+                        margin: "3vw auto"
+                    }}>
+                        <FoodCard menu={menu} onClick={function (): void {
+                            menu.isBigSize === true && props.setChosenMenu({
+                                title: menu.title,
+                                description: menu.description,
+                                price: menu.price,
+                                id: menu.id,
+                                image: menu.image,
+                                category: menu.category,
+                                isBigSize: menu.isBigSize,
+                                bigSizeDiffPrice: menu.bigSizeDiffPrice,
+                                isStatus: menu.isStatus,
+                                isSale: menu.isSale,
+                            });
+                            menu.isBigSize === false && props.setChosenMenu(menu);
+                            props.setDetailDialogOpen(true);
+                        }} />
+
+                    </Grid>
+                )
+            }
+            )}
+        </Grid>
+    )
+}
