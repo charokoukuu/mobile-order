61055d9888cfb015f8a003a314ebaae9475d70e9
charokoukuu <charokoukuu@gmail.com>
2022-09-29 13:32:10 +0900

    color情報のjson化

---
+++ src/api/Colors.ts
@@ -0,0 +1,24 @@
+interface colorType {
+    surface: string,
+    text: string,
+    background: string,
+    price: string
+}
+export const LightTheme: colorType = {
+    surface: "#006C9B",
+    text: "#707070",
+    background: "#FFFFFF",
+    price: "#F8B500"
+}
+
+export const DarkTheme: colorType = {
+    surface: "#000000",
+    text: "#076D80",
+    background: "#FFFFFF",
+    price: "#F8B500"
+}
+
+export const Color: () => string = () => {
+    (!window.localStorage.getItem('theme')) && window.localStorage.setItem('theme', 'light');
+    return LightTheme.surface || "";
+}
