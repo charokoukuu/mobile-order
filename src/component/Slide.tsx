149eb7cf30315ab3bc48d26e663de163214e7970
nikudango <satoru_rutosa@icloud.com>
2022-10-10 22:47:33 +0900

    margin : "0 auto"

--- src/component/Slide.tsx
+++ src/component/Slide.tsx
@@ -41,7 +41,7 @@ export default function Slide() {
         return (
           <div key={index} >
             <p style={{ margin: "5% 10%", color: "#000000" }}>{slide[0] as string}</p>
-            <div style={{top:"0px",left:"50%"}}>
+            <div style={{margin:"0 auto"}}>
               {slide[1]}
             </div>
           </div>
