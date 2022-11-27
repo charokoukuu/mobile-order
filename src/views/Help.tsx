4d91339e4070c21ae856cb822cfe436010855059
Nikudanngo <satoru_rutosa@icloud.com>
2022-11-27 16:29:23 +0900

    Helpのリプレイス

--- src/views/Help.tsx
+++ src/views/Help.tsx
@@ -3,40 +3,47 @@ export const Help = (props: { appBarHeight: number }) => {
   return (
     <div style={{ margin: "auto", textAlign: "center" }}>
       <Spacer appBarHeight={props.appBarHeight} mode={"history"} />
-      <div
-        style={{
-          backgroundColor: "#ffffff",
-          padding: "2vw",
-          borderRadius: "8px",
-          width: "90%",
-          margin: "2vw auto",
-        }}
-      >
-        <h2 className="themeFontColor">STEP 1</h2>
-        <p>メニューを選択します</p>
-        <img src="/help/home.png" alt="menu" width={"70%"} />
-        <h2 className="themeFontColor">STEP 2</h2>
-        <p>オプションを選択し、カートに追加します</p>
-        <img src="/help/detail.png" alt="menu" width={"70%"} />
-        <h2 className="themeFontColor">STEP 3</h2>
-        <p>決済方法を選択します</p>
-        <img src="/help/order.png" alt="menu" width={"70%"} />
-        <h2 className="themeFontColor">STEP 4</h2>
-        <p style={{ width: "70vw", margin: "auto" }}>
+      <div className="m-[2%] rounded-b-lg bg-white p-[2%]">
+        <h2 className="my-4 text-2xl font-bold text-runticketBlue">STEP 1</h2>
+        <p className="mb-3">メニューを選択します</p>
+        <img
+          className="mx-auto"
+          src="/help/home.png"
+          alt="menu"
+          width={"70%"}
+        />
+        <h2 className="my-4 text-2xl font-bold text-runticketBlue">STEP 2</h2>
+        <p className="mb-3">オプションを選択し、カートに追加します</p>
+        <img
+          className="mx-auto"
+          src="/help/detail.png"
+          alt="menu"
+          width={"70%"}
+        />
+        <h2 className="my-4 text-2xl font-bold text-runticketBlue">STEP 3</h2>
+        <p className="mb-3">決済方法を選択します</p>
+        <img
+          className="mx-auto"
+          src="/help/order.png"
+          alt="menu"
+          width={"70%"}
+        />
+        <h2 className="my-4 text-2xl font-bold text-runticketBlue">STEP 4</h2>
+        <p className="m-auto mb-3 w-[70%]">
           発行されたQRコードをスキャンし、食券を発行します
         </p>
-        <div style={{ display: "flex", margin: "5vw 0 13vw 0" }}>
+        <div className="m-[5%_0_13%_0] flex">
           <img
             src="/help/printer.svg"
             alt="menu"
             width={"50%"}
-            style={{ transform: "rotate(-10deg)" }}
+            className="rotate-[-10deg] transform"
           />
           <img
             src="/help/scanner.svg"
             alt="menu"
             width={"50%"}
-            style={{ transform: "rotate(-10deg)" }}
+            className="rotate-[-10deg] transform"
           />
         </div>
       </div>
