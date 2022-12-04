416e4c718e813d4d8d89a17ce076797689f40f5e
charokoukuu <charokoukuu@gmail.com>
2022-12-05 02:57:00 +0900

    test

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,19 @@ import "./views/styles/index.css";
 import reportWebVitals from "./reportWebVitals";
 import Router from "./router";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://c70cec8043cf4343b227e3a17087e16d@o4504265411526656.ingest.sentry.io/4504265413820416",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
