fa26184825f142edc27285bfad3def28c9c0b4e4
Nikudanngo <satoru_rutosa@icloud.com>
2023-01-22 03:52:21 +0900

    OrderCompletedのリプレイスとエラーハンドリングの修正

--- src/views/OrderCompleted.tsx
+++ src/views/OrderCompleted.tsx
@@ -4,7 +4,7 @@ import { Link, useParams } from "react-router-dom";
 import { GetSpecificData } from "../api/SubmitGet";
 import { onSnapshot, doc } from "firebase/firestore";
 import { QRCodeSVG } from "qrcode.react";
-import { Button, Card } from "@mui/material";
+import { Button, Card, Divider } from "@mui/material";
 import { LoadingAnimation } from "../component/LoadingAnimation";
 import Slide from "../component/Slide";
 import { auth } from "../api/Firebase";
@@ -20,13 +20,20 @@ export const OrderCompleted = ({ appBarHeight }: Props) => {
   const [orderData, setOrderData] = useState<OrderData>();
   const [isGetOrderData, setIsGetOrderData] = useState<boolean>(false);
   const [isChangeStatus, setIsChangeStatus] = useState<boolean>(false);
+  const [isPramsIdError, setIsPramsIdError] = useState<boolean>(false);
   const params = useParams();
 
   useEffect(() => {
     (async () => {
-      params.id &&
-        setOrderData((await GetSpecificData("order", params.id)) as OrderData);
-      setIsGetOrderData(true);
+      if (params.id) {
+        const orderData = await GetSpecificData("order", params.id);
+        if (orderData !== null) {
+          setOrderData(orderData);
+        } else {
+          setIsPramsIdError(true);
+        }
+        setIsGetOrderData(true);
+      }
     })();
     onSnapshot(doc(db, "order", params.id || ""), (doc) => {
       if (isChecked) {
@@ -44,7 +51,7 @@ export const OrderCompleted = ({ appBarHeight }: Props) => {
   return (
     <>
       <Spacer appBarHeight={appBarHeight} mode={"history"} />
-      {params.status === "success" ? (
+      {params.id && params.status === "success" ? (
         <IntegrationNotistack message="決済が完了しました" variant="success" />
       ) : (
         params.status === "failed" && (
@@ -54,122 +61,53 @@ export const OrderCompleted = ({ appBarHeight }: Props) => {
           />
         )
       )}
-      <div style={{ margin: "10px 0" }}>
+      <div className="mt-3">
         {isGetOrderData ? (
-          <Card
-            style={{
-              marginTop: "20px",
-              margin: "auto",
-              maxWidth: "1200px",
-              width: "90%",
-              padding: "10% 0",
-            }}
-          >
-            {auth.currentUser?.uid === orderData?.user.uid ? (
+          <Card className="mx-auto mt-5 w-[90%] max-w-[1200px] py-3">
+            {auth.currentUser?.uid === orderData?.user.uid ||
+            !isPramsIdError ? (
               <>
                 {/* <p>{orderData?.id}</p> */}
                 {(orderData?.isStatus === "ordered" ||
                   orderData?.isStatus === "cooked") && (
                   <div>
-                    <div style={{ display: "flex", height: "10vh" }}>
-                      <h2
-                        style={{
-                          margin: "0 auto",
-                          marginTop: "10px",
-                          textAlign: "center",
-                          color: "#000000",
-                        }}
-                      >
-                        食券発行用QR
-                      </h2>
-                    </div>
-                    <div
-                      style={{
-                        position: "relative",
-                        width: "100%",
-                        height: "100%",
-                        padding: "80px 0",
-                      }}
-                    >
+                    <h2 className="my-3 text-center font-sans text-xl text-black">
+                      食券発行用QR
+                    </h2>
+                    <div className="relative h-full w-full py-20">
                       <QRCodeSVG
-                        style={{
-                          position: "absolute",
-                          top: "0%",
-                          right: "50%",
-                          transform: "translate(50%, 0)",
-                        }}
+                        className="absolute top-0 right-1/2 translate-x-1/2 transform"
                         value={orderData?.id}
                         size={150}
                       />
                     </div>
-                    <h2
-                      className="japanese_L"
-                      style={{
-                        textAlign: "center",
-                        fontSize: "20px",
-                        marginTop: "5px",
-                        color: "#000000",
-                      }}
-                    >
+                    <p className="mt-1 text-center text-xl text-black">
                       {`注文ID: ${orderData?.id}`}
-                    </h2>
+                    </p>
                   </div>
                 )}
                 {orderData?.isStatus === "complete" && (
-                  <div
-                    className="japanese_L"
-                    style={{
-                      textAlign: "center",
-                      fontSize: "120%",
-                      margin: "3% 0",
-                    }}
-                  >
+                  <p className="my-2 text-center text-xl">
                     ご注文ありがとうございました
-                  </div>
+                  </p>
                 )}
-                <h2
-                  style={{
-                    textAlign: "center",
-                    margin: "0 auto",
-                    color: "#000000",
-                    fontSize: "30px",
-                  }}
-                >
+                <h2 className="my-3 text-center text-3xl text-black">
                   {`￥${orderData?.totalPrice}`}
                 </h2>
-                <div style={{ margin: "5% 0" }}>
+                <div className="my-4">
                   {orderData?.menu.map((e: MenuData, i: number) => {
                     return (
                       <div
-                        style={{
-                          display: "table",
-                          width: "50%",
-                          margin: "0 auto",
-                          color: "rgba(0, 0, 0, 0.65)",
-                        }}
+                        className="grid grid-cols-2 gap-4 text-black opacity-[0.65]"
                         key={i}
                       >
-                        <span
-                          style={{
-                            display: "table-cell",
-                            verticalAlign: "left",
-                            color: "#000000",
-                          }}
-                        >
-                          {e.title}
-                        </span>
-                        <span
-                          style={{
-                            display: "table-cell",
-                            textAlign: "right",
-                            color: "#000000",
-                          }}
-                        >{`￥${e.price}`}</span>
+                        <span className="grid justify-end">{e.title}</span>
+                        <span className="grid justify-start">{`￥${e.price}`}</span>
                       </div>
                     );
                   })}
                   {orderData?.isStatus === "not_payed" && (
-                    <div style={{ textAlign: "center", marginTop: "10vw" }}>
+                    <div className="mt-3 text-center">
                       <Button
                         variant="contained"
                         onClick={() => {
@@ -183,32 +121,16 @@ export const OrderCompleted = ({ appBarHeight }: Props) => {
                     </div>
                   )}
                   {params.status === "failed" && (
-                    <div style={{ margin: "30px 0" }}>
-                      <h2
-                        style={{
-                          textAlign: "center",
-                          fontSize: "20px",
-                          width: "80vw",
-                          margin: "auto",
-                        }}
-                      >
+                    <div className="my-7">
+                      <h2 className="mx-auto w-5/6 text-center text-xl">
                         決済情報が見つかりませんでした。以下に表示されたコードを問い合わせフォームに記載してください。
                       </h2>
-                      <div style={{ backgroundColor: "rgba(0,0,0,0.8)" }}>
-                        <p
-                          style={{
-                            textAlign: "left",
-                            fontSize: "20px",
-                            width: "80vw",
-                            margin: "30px auto",
-                            overflowWrap: "break-word",
-                            color: "#ffffff",
-                          }}
-                        >
+                      <div className="bg-black/80">
+                        <p className="my-8 mx-auto w-5/6 break-words text-start text-xl text-white">
                           {params.id}
                         </p>
                       </div>
-                      <div style={{ textAlign: "center" }}>
+                      <div className="flex justify-center">
                         <Button
                           variant="contained"
                           href={`https://docs.google.com/forms/d/e/1FAIpQLSfRRIK0WBAoMt_WN3RAKbP598LZOQAhsOrIQu8O7eAZE81x1Q/viewform?usp=pp_url&entry.951689160=${params.id}`}
@@ -220,8 +142,9 @@ export const OrderCompleted = ({ appBarHeight }: Props) => {
                   )}
                   {(orderData?.isStatus === "ordered" ||
                     orderData?.isStatus === "cooked") && (
-                    <div style={{ margin: "10% 0" }}>
-                      <h2 style={{ textAlign: "center", color: "#000000" }}>
+                    <div className="my-4">
+                      <Divider />
+                      <h2 className="my-3 text-center font-sans text-xl text-black">
                         食券受け取り方法
                       </h2>
                       <Slide />
@@ -234,19 +157,28 @@ export const OrderCompleted = ({ appBarHeight }: Props) => {
               </>
             ) : (
               <>
-                <div style={{ margin: "0 10%", textAlign: "center" }}>
-                  <h1>
+                <div className="mx-2 text-center">
+                  <h1 className="text-4xl">
                     404 Not Found.
                     <br />
                   </h1>
-                  <h2>申し訳ありません．</h2>
                   <p>
-                    {auth.currentUser?.displayName}
-                    さんのお探しの注文が見つかりませんでした。
+                    <span className="text-xl">
+                      {auth.currentUser?.displayName}
+                      さんの
+                      <br />
+                      注文が見つかりませんでした。
+                    </span>
                     <br />
                     もし、注文後正しくQRコードが発行されなかった場合は、
+                    アカウントをご確認の上、
                     以下の問い合わせボタンからお問い合わせください。
                   </p>
+                  <div className="bg-black/80">
+                    <p className="my-8 mx-auto w-5/6 break-words text-start text-xl text-white">
+                      {params.id}
+                    </p>
+                  </div>
                   <div>
                     <Button variant="text" component={Link} to="/history">
                       注文履歴に戻る
@@ -254,7 +186,7 @@ export const OrderCompleted = ({ appBarHeight }: Props) => {
                     <span>/</span>
                     <Button
                       variant="text"
-                      href="https://docs.google.com/forms/d/e/1FAIpQLSfRRIK0WBAoMt_WN3RAKbP598LZOQAhsOrIQu8O7eAZE81x1Q/viewform"
+                      href={`https://docs.google.com/forms/d/e/1FAIpQLSfRRIK0WBAoMt_WN3RAKbP598LZOQAhsOrIQu8O7eAZE81x1Q/viewform?usp=pp_url&entry.951689160=${params.id}`}
                     >
                       問い合わせる
                     </Button>
