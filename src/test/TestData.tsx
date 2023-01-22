6f646b92a4819fe00715fcf3eb2089736e3f0d8d
Nikudanngo <satoru_rutosa@icloud.com>
2023-01-22 15:01:16 +0900

    comment out no use

--- src/test/TestData.tsx
+++ src/test/TestData.tsx
@@ -1,25 +1,25 @@
-import { httpsCallable } from "firebase/functions";
+// import { httpsCallable } from "firebase/functions";
 import { useState } from "react";
-import { functions } from "../api/Firebase";
+// import { functions } from "../api/Firebase";
 import { RedirectModal } from "../component/RedirectModal";
 import { Spacer } from "../component/SwipeTabs";
 
 export const TestData = (props: { appBarHeight: number }) => {
   const [isModal, setIsModal] = useState<boolean>(false);
-  const Pay = () => {
-    const paypay = httpsCallable(functions, "test");
-    (async () => {
-      // PayPayの型がわからないので一旦disable
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const data: any = await paypay({
-        orderId: "ljnvkjdnjdfnjklsf",
-        redirectUrl: "https://mobile-order-4d383.web.app",
-        amount: 300,
-        orderDescription: "test",
-      });
-      console.log(data);
-    })();
-  };
+  // const Pay = () => {
+  //   const paypay = httpsCallable(functions, "test");
+  //   (async () => {
+  //     // PayPayの型がわからないので一旦disable
+  //     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  //     const data: any = await paypay({
+  //       orderId: "ljnvkjdnjdfnjklsf",
+  //       redirectUrl: "https://mobile-order-4d383.web.app",
+  //       amount: 300,
+  //       orderDescription: "test",
+  //     });
+  //     console.log(data);
+  //   })();
+  // };
   return (
     <div>
       <Spacer appBarHeight={props.appBarHeight} mode={"history"} />
