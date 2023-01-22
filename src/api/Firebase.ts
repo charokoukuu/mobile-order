922687b3e7ef20552d9e411fdabe08fac538742a
Nikudanngo <satoru_rutosa@icloud.com>
2023-01-22 17:12:33 +0900

    _

--- src/api/Firebase.ts
+++ src/api/Firebase.ts
@@ -9,14 +9,14 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBB2z8O3h9bTXmb3mt0I_QyQlysO-XW-D4",
-  authDomain: "mobile-order-4d383.firebaseapp.com",
-  databaseURL: "https://mobile-order-4d383-default-rtdb.firebaseio.com",
-  projectId: "mobile-order-4d383",
-  storageBucket: "mobile-order-4d383.appspot.com",
-  messagingSenderId: "1029313752045",
-  appId: "1:1029313752045:web:b4b6ac5a8aafc6a17d55e4",
-  measurementId: "G-MGHD5LQRPP",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const admin = initializeApp(firebaseConfig);
