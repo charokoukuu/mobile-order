53c5b6a76908ec69d11435872d042049e1627ec1
えんどー <64085231+Nikudanngo@users.noreply.github.com>
2023-02-26 16:41:48 +0900

    Update src/component/Order.tsx
    Co-authored-by: charokoukuu <35647163+charokoukuu@users.noreply.github.com>

--- src/component/Order.tsx
+++ src/component/Order.tsx
@@ -76,7 +76,7 @@ export const Order = (props: OrderProps) => {
         <Box />
         <div className="w-full bg-[#eeece4]">
           <div
-            className={classNames("mx-auto flex overflow-x-auto py-5", {
+            className={classNames("box mx-auto flex overflow-x-auto py-5", {
               "max-w-4xl": orderCount.length <= 4,
             })}
           >
