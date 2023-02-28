2c9dc315c51df197896a50896d0cc344462a6169
Nikudanngo <satoru_rutosa@icloud.com>
2023-02-28 15:20:26 +0900

    react-swipeable-viewsからSwiperへのリプレイス
    - 処理のメモ化

--- src/component/SwipeTabs.tsx
+++ src/component/SwipeTabs.tsx
@@ -6,6 +6,10 @@ import { Grid } from "@mui/material";
 import { MenuData } from "../types";
 import { CategoryProp } from "../views/Menu";
 import styled from "@emotion/styled";
+import { Swiper, SwiperSlide } from "swiper/react";
+import type { Swiper as SwiperCore } from "swiper";
+import "swiper/css";
+import { useMemo, useRef, useState } from "react";
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -13,7 +17,6 @@ interface TabPanelProps {
   index: number;
   value: number;
 }
-
 interface SwipeTabsProps {
   menu: MenuData[];
   category: CategoryProp[];
@@ -49,14 +52,23 @@ function a11yProps(index: number) {
 }
 
 export default function SwipeTabs(props: SwipeTabsProps) {
-  const [value, setValue] = React.useState(0);
   const menuCategoryArray: CategoryProp[] = props.category;
+  const [value, setValue] = useState(0);
+  // const [swiper, setSwiper] = useState<SwiperCore | null>(null);
+  const swiperRef = useRef<SwiperCore | null>(null);
+
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
+    // swiper?.slideTo(newValue);
+    swiperRef.current?.slideTo(newValue);
   };
-
-  const handleChangeIndex = (index: number) => {
-    setValue(index);
+  const onSwiper = (currentSwiper: SwiperCore) => {
+    const swiperInstance = currentSwiper;
+    // setSwiper(swiperInstance);
+    swiperRef.current = swiperInstance;
+  };
+  const onSlideChange = (currentSwiper: SwiperCore) => {
+    setValue(currentSwiper.activeIndex);
   };
 
   return (
@@ -71,59 +83,63 @@ export default function SwipeTabs(props: SwipeTabsProps) {
           aria-label="full width tabs example"
         >
           {menuCategoryArray.map((e, i) => {
-            return <Tab key={e} label={e} {...a11yProps(i)} />;
+            return <Tab key={i} label={e} {...a11yProps(i)} />;
           })}
         </Tabs>
       </SwipeTabsWapper>
-      {/* <SwipeableViews
-        className="mx-auto max-w-[730px] py-1"
-        index={value}
-        onChangeIndex={handleChangeIndex}
-      > */}
-      {menuCategoryArray.map((category: string, index: number) => {
-        return (
-          <TabPanel value={value} index={index} key={index}>
-            <FilterMenuData
-              menu={props.menu}
-              categoryMode={category}
-              setChosenMenu={props.setChosenMenu}
-              setDetailDialogOpen={props.setDetailDialogOpen}
-              category={props.category}
-            />
-          </TabPanel>
-        );
-      })}
-      {/* </SwipeableViews> */}
+      <Swiper
+        simulateTouch={false}
+        spaceBetween={50}
+        slidesPerView={1}
+        onSwiper={onSwiper}
+        onSlideChange={onSlideChange}
+      >
+        {menuCategoryArray.map((category: string, index: number) => {
+          return (
+            <SwiperSlide key={index}>
+              <TabPanel value={value} index={index}>
+                <FilterMenuData
+                  menu={props.menu}
+                  categoryMode={category}
+                  setChosenMenu={props.setChosenMenu}
+                  setDetailDialogOpen={props.setDetailDialogOpen}
+                  category={props.category}
+                />
+              </TabPanel>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </div>
   );
 }
 
 const FilterMenuData = (props: FilterMenuDataProps) => {
+  const filteredMenu = useMemo(() => {
+    return props.menu.filter(
+      (item: MenuData) => item.category === props.categoryMode && item.isStatus
+    );
+  }, [props.categoryMode, props.menu]);
   return (
     <Grid padding={1.5} container spacing={1.5} justifyContent="center">
       <Spacer appBarHeight={props.appBarHeight || 56} mode={"menu"} />
-      {props.menu
-        .filter(
-          (item: MenuData) =>
-            item.category === props.categoryMode && item.isStatus
-        )
-        .map((menu: MenuData, index: number) => {
-          return (
-            <Grid item key={index} xs={6} sm={4}>
-              <FoodCard
-                menu={menu as MenuData}
-                deleteButton={false}
-                onClick={function (): void {
-                  menu.isBigSize === true &&
-                    props.setChosenMenu(menu as MenuData);
-                  menu.isBigSize === false &&
-                    props.setChosenMenu(menu as MenuData);
-                  props.setDetailDialogOpen(true);
-                }}
-              />
-            </Grid>
-          );
-        })}
+      {filteredMenu.map((menu: MenuData, index: number) => {
+        return (
+          <Grid item key={index} xs={6} sm={4}>
+            <FoodCard
+              menu={menu as MenuData}
+              deleteButton={false}
+              onClick={function (): void {
+                menu.isBigSize === true &&
+                  props.setChosenMenu(menu as MenuData);
+                menu.isBigSize === false &&
+                  props.setChosenMenu(menu as MenuData);
+                props.setDetailDialogOpen(true);
+              }}
+            />
+          </Grid>
+        );
+      })}
     </Grid>
   );
 };
@@ -134,7 +150,7 @@ const SwipeTabsWapper = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  z-index: 1;
+  z-index: 10;
   pointer-events: none;
 `;
 
