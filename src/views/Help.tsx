94e80c594e8b498e694e185fb33ede8ea07704c3
charokoukuu <charokoukuu@gmail.com>
2023-03-19 22:46:34 +0900

    キャッシュを無効にするためにランダムなクエリを付与する

--- src/views/Help.tsx
+++ src/views/Help.tsx
@@ -1,12 +1,19 @@
 import { Spacer } from "../component/SwipeTabs";
 import Iframe from "react-iframe";
+import { useEffect, useState } from "react";
+import { RandomID } from "../api/helper";
 export const Help = (props: { appBarHeight: number }) => {
+  const [randomQuery, setRandomQuery] = useState<string>("");
+  useEffect(() => {
+    setRandomQuery(RandomID());
+  }, []);
   return (
     <div className="mt-4 max-w-3xl text-center">
       <Spacer appBarHeight={props.appBarHeight} mode={"history"} />
       <Iframe
         id="page1"
-        url="https://runticket-law.web.app/help.html?a=5"
+        // キャッシュを無効にするためにランダムなクエリを付与する
+        url={"https://runticket-law.web.app/help.html?query=" + randomQuery}
         position="absolute"
         className="mx-auto max-w-3xl rounded-lg bg-white p-[3%]"
         width="90%"
