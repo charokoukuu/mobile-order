8d063153c3a29298d26bd19bf7b5a2d8baa1304e
charokoukuu <charokoukuu@gmail.com>
2023-03-19 22:05:04 +0900

    padding

--- src/views/Help.tsx
+++ src/views/Help.tsx
@@ -2,15 +2,15 @@ import { Spacer } from "../component/SwipeTabs";
 import Iframe from "react-iframe";
 export const Help = (props: { appBarHeight: number }) => {
   return (
-    <div className="m-4 mx-auto h-[100vh] max-w-3xl text-center">
+    <div className="mt-4 max-w-3xl text-center">
       <Spacer appBarHeight={props.appBarHeight} mode={"history"} />
       <Iframe
         id="page1"
         url="https://runticket-law.web.app/help.html?a=5"
         position="absolute"
-        className="mx-auto max-w-3xl rounded-lg bg-white p-[2%] [&_h2]:my-[0.83em] [&_h2]:text-[1.5em] [&_h2]:font-bold [&_li]:list-decimal [&_ul]:ml-4 [&_ul_ul>li]:list-[lower-latin] [&_ul_ul_ul>li]:ml-4 [&_ul_ul_ul>li]:list-[cjk-ideographic]"
+        className="mx-auto max-w-3xl rounded-lg bg-white p-[3%]"
         width="90%"
-        height="100%"
+        height="90%"
         styles={{
           left: "50%",
           transform: "translateX(-50%)",
